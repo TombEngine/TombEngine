cbfdc90da091c3d50c4dfee963cf4e5070c62347
Lwmte <3331699+Lwmte@users.noreply.github.com>
2025-05-12 21:43:46 +0200

    Extend GetAnalogKeyValue description, add changelog entry

--- TombEngine/Scripting/Internal/TEN/Input/InputHandler.cpp
+++ TombEngine/Scripting/Internal/TEN/Input/InputHandler.cpp
@@ -30,6 +30,8 @@ namespace TEN::Scripting::Input
 	}
 
 	/// Get the analog value of an action key.
+	// Returns either 0 or 1 for digital key mappings (e.g. keyboard or gamepad buttons),
+	// but may return arbirtrary values for analog key mappings (e.g. gamepad or mouse axes).
 	// @function GetAnalogKeyValue
 	// @tparam Input.ActionID actionID Action ID to query.
 	// @treturn float Analog value in the range [0, 1].
