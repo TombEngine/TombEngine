1f1a6cad3e2329779c9d53bda31a0cd3ef7707ec
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2025-05-29 13:05:52 -0400

    docs

--- TombEngine/Scripting/Internal/TEN/Inventory/InventoryHandler.cpp
+++ TombEngine/Scripting/Internal/TEN/Inventory/InventoryHandler.cpp
@@ -26,7 +26,7 @@ namespace TEN::Scripting::InventoryHandler
 	//@tparam Objects.ObjID objectID Object ID of the item to add.
 	//@tparam[opt=1] int count The amount of items to add. Default is the yield from a single pickup, e.g. 1 from a medipack, 12 from a flare pack.
 	//@tparam[opt=false] bool addToPickupSummary If true, display the item in the pickup summary. Default is false.
-	//@tparam[opt=Vec2(50&#44; 50)] Vec2 startPosition Screen position in pixels where object should start from. Default is middle of the screen.
+	//@tparam[opt=Vec2(50&#44; 50)] Vec2 startPosition Screen position in percentage, where the pickup object should start from. Default is middle of the screen.
 	static void GiveItem(GAME_OBJECT_ID objectID, TypeOrNil<int> count, TypeOrNil<bool> addToPickupSummary, TypeOrNil<Vec2> startPosition)
 	{
 		auto convertedCount = ValueOr<int>(count, 1);
