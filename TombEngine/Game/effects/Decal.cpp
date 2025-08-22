9e12c1004ccedb0f52293dbc1f151d7a18239691
Lwmte <3331699+Lwmte@users.noreply.github.com>
2025-08-22 20:07:17 +0200

    Update Decal.cpp

--- TombEngine/Game/effects/Decal.cpp
+++ TombEngine/Game/effects/Decal.cpp
@@ -12,7 +12,7 @@ namespace TEN::Effects::Decal
 	void SpawnDecal(Vector3 pos, int roomNumber, DecalType type)
 	{
 		auto distance = Vector3::Distance(Camera.pos.ToVector3(), pos);
-		if (!Lara.Control.Look.IsUsingLasersight && distance > COLLISION_CHECK_DISTANCE)
+		if (type == DecalType::BulletHole && !Lara.Control.Look.IsUsingLasersight && distance > COLLISION_CHECK_DISTANCE)
 			return;
 
 		auto& decal = GetNewEffect(Decals, Decal::COUNT_MAX);
