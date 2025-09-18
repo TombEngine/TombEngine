0cef4117cec08766fd3f42476a01f99d212ce4ef
Nemoel-Tomo <tomo_669@hotmail.com>
2025-09-18 20:58:20 +0200

    bla

--- TombEngine/Game/Lara/lara.cpp
+++ TombEngine/Game/Lara/lara.cpp
@@ -156,19 +156,19 @@ void LaraControl(ItemInfo* item, CollisionInfo* coll)
 	auto offset = GetJointPosition(item, LM_HEAD, Vector3i(0, -4, 64));
 
 	auto pos1 = GetJointPosition(item, LM_HEAD, Vector3i(38, -45, 0));
-	auto pos2 = GetJointPosition(item, LM_HEAD, Vector3i(38, -45, -450));
+	auto pos2 = GetJointPosition(item, LM_HEAD, Vector3i(38, -45, 450));
 
 	auto orient = Geometry::GetOrientToPoint(pos1.ToVector3(), pos2.ToVector3());
 	//auto orient = Geometry::GetOrientToPoint(origin, target)
 
-	TEN::Entities::Traps::EmitTransientLaserBeam(GameVector(pos1, item->RoomNumber), orient, 3, Vector4(1.0f, 0.0f, 0.0f, 1.0f), true, false, false);
+	TEN::Entities::Traps::EmitTransientLaserBeam(GameVector(pos1, item->RoomNumber), orient, 3, Vector4(1.0f, 0.0f, 0.0f, 1.0f), true, true, false);
 
 	 pos1 = GetJointPosition(item, LM_RHAND, Vector3i(0, 0, 0));
 	 pos2 = GetJointPosition(item, LM_RHAND, Vector3i(0, 0, 450));
 
 	 orient = Geometry::GetOrientToPoint(pos1.ToVector3(), pos2.ToVector3());
 
-	TEN::Entities::Traps::EmitTransientLaserBeam( GameVector(pos1, item->RoomNumber), orient, 2, Vector4(1.0f, 0.0f, 0.0f, 1.0f), true, false, false);
+	TEN::Entities::Traps::EmitTransientLaserBeam( GameVector(pos1, item->RoomNumber), orient, 2, Vector4(1.0f, 0.0f, 0.0f, 1.0f), true, true, false);
 
 	// Handle object interation adjustment parameters.
 	if (player.Control.IsMoving)
