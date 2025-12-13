40408c84c40ae25a4695b83130398dccb6ff036b
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2025-12-13 17:04:48 -0500

    Fix as per AnimRefactor5 branch

--- TombEngine/Scripting/Internal/TEN/Objects/Lara/LaraObject.cpp
+++ TombEngine/Scripting/Internal/TEN/Objects/Lara/LaraObject.cpp
@@ -531,7 +531,6 @@ void LaraObject::Interact(const Moveable& mov, TypeOrNil<int> animNumber,
 		ResetPlayerFlex(_moveable);
 		SetAnimation(_moveable, convertedAnimNumber);
 
-		_moveable->Animation.FrameNumber = 0;
 		player.Control.IsMoving = false;
 		player.Control.HandStatus = HandStatus::Busy;
 	}
