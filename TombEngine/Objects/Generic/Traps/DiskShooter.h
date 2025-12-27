8acee6f07d14125489f061e2cc8781ecc9298e7e
Nemoel-Tomo <tomo_669@hotmail.com>
2025-12-27 23:14:29 +0100

    Added DiskShooter

---
+++ TombEngine/Objects/Generic/Traps/DiskShooter.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace TEN::Entities::Traps
+{
+	void InitializeDiskShooter(short itemNumber);
+	void ControlDisk(short itemNumber);
+	void ControlDiskShooter(short itemNumber);
+
+	void SpawnDartSmoke(const Vector3& pos, const Vector3& vel, bool isHit);
+}
