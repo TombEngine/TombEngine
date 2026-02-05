5fcd2b97407050c4f0fbe9e5e401e8fe27e4467e
Lwmte <3331699+Lwmte@users.noreply.github.com>
2026-02-05 18:41:37 +0100

    Update LaserBarrier.cpp

--- TombEngine/Objects/TR5/Trap/LaserBarrier.cpp
+++ TombEngine/Objects/TR5/Trap/LaserBarrier.cpp
@@ -103,9 +103,9 @@ namespace TEN::Entities::Traps
 		SpawnDynamicLight(
 			item.Pose.Position.x, item.Pose.Position.y, item.Pose.Position.z,
 			8,
-			intensityNorm * (item.Model.Color.x / 2),
-			intensityNorm * (item.Model.Color.y / 2),
-			intensityNorm * (item.Model.Color.z / 2));
+			intensityNorm * (item.Model.Color.x),
+			intensityNorm * (item.Model.Color.y),
+			intensityNorm * (item.Model.Color.z));
 	}
 
 	void ControlLaserBarrier(short itemNumber)
