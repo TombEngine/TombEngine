00bdb786b5a0196cdfcd1f6a97cbff0fbe5fc7dd
AlexxxQ <152021684+AlexxxQ@users.noreply.github.com>
2026-03-27 01:18:47 +0300

    Update GBuffer.hlsl
    change input.Pos to faster pos.x

--- TombEngine/Shaders/GBuffer.hlsl
+++ TombEngine/Shaders/GBuffer.hlsl
@@ -65,7 +65,7 @@ float4 screenPos = mul(float4(pos, 1.0f), ViewProjection);
 if (CameraUnderwater != Water)
 {
     float dist = length(pos - CamPositionWS.xyz);
-    float factor = InterpolatedFrame + (input.Position.x + input.Position.z) * 0.2f;
+    float factor = InterpolatedFrame + (pos.x + pos.z) * 0.2f;
     float xOffset = (sin(factor * PI / 20.0f)) * (dist / 1024) * 3;
     float yOffset = (cos(factor * PI / 20.0f)) * (dist / 1024) * 3;
     screenPos.x += xOffset * weight;
