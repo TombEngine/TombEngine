e1a4952743a7bfb4656f5cfb30b616f9c8f74cee
Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
2026-03-29 21:46:44 +0100

    Reverse accidental editing of non-PR related files

--- TombEngine/Game/effects/drip.h
+++ TombEngine/Game/effects/drip.h
@@ -34,8 +34,8 @@ namespace TEN::Effects::Drip
 
 	extern std::vector<Drip> Drips;
 
-	void SpawnDrip(const Vector3& pos, int roomNumber, const Vector3& velocity, float lifeInSec, float gravity, float sizeScale = 1.0f);
-	void SpawnSplashDrips(const Vector3& pos, int roomNumber, unsigned int count, bool isSmallSplash = false, float splashScale = 1.0f);
+	void SpawnDrip(const Vector3& pos, int roomNumber, const Vector3& velocity, float lifeInSec, float gravity);
+	void SpawnSplashDrips(const Vector3& pos, int roomNumber, unsigned int count, bool isSmallSplash = false);
 	void SpawnWetnessDrip(const Vector3& pos, int roomNumber);
 
 	void UpdateDrips();
