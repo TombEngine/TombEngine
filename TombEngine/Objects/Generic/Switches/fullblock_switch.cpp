d0b9ebda6c07ab9efed72ca06e04f54e7906c6f3
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2026-04-30 13:15:11 -0400

    increase tolerance

--- TombEngine/Objects/Generic/Switches/fullblock_switch.cpp
+++ TombEngine/Objects/Generic/Switches/fullblock_switch.cpp
@@ -23,7 +23,7 @@ namespace TEN::Entities::Switches
 		GameBoundingBox(
 			-384, 384,
 			0, CLICK(1),
-			0, BLOCK(0.5f)
+            -CLICK(1), BLOCK(0.5f)
 		),
 		std::pair(
 			EulerAngles(ANGLE(-10.0f), ANGLE(-30.0f), ANGLE(-10.0f)),
