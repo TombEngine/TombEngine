e2a724cdb8347c297b1c94ed9ccdb506c56c10bb
TrainWrack <120750885+TrainWrack@users.noreply.github.com>
2026-05-09 12:03:34 -0400

    Fix

--- TombEngine/Scripting/Internal/TEN/View/ViewHandler.cpp
+++ TombEngine/Scripting/Internal/TEN/View/ViewHandler.cpp
@@ -198,9 +198,9 @@ namespace TEN::Scripting::View
 		return (screenRes.x / screenRes.y);
 	}
 
-	static std::tuple<PostProcessMode, float, ScriptColor> GetPostProcess()
+	static std::tuple<PostProcessMode, float> GetPostProcess()
 	{
-		return std::make_tuple(g_Renderer.GetPostProcessMode(), g_Renderer.GetPostProcessStrength(), (ScriptColor)g_Renderer.GetPostProcessTint());
+		return std::make_tuple(g_Renderer.GetPostProcessMode(), g_Renderer.GetPostProcessStrength());
 	}
 
 	static void SetPostProcess(PostProcessMode mode, TypeOrNil<float> strength)
