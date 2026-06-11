bf6fc58d1e97346114efea71be4e46b4e4fadc95
Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
2026-06-11 22:48:52 +0100

    Add quadbike smoke start/end color properties

--- TombEngine/Objects/TR3/Vehicles/quad_bike.cpp
+++ TombEngine/Objects/TR3/Vehicles/quad_bike.cpp
@@ -20,6 +20,7 @@
 #include "Objects/TR3/Vehicles/quad_bike_info.h"
 #include "Objects/Utils/VehicleHelpers.h"
 #include "Scripting/Include/Flow/ScriptInterfaceFlowHandler.h"
+#include "Scripting/Internal/TEN/Properties/PropertyHandler.h"
 #include "Sound/sound.h"
 #include "Specific/level.h"
 #include "Specific/Input/Input.h"
@@ -1024,19 +1025,21 @@ namespace TEN::Entities::Vehicles
 		return drive;
 	}
 
-	static void TriggerQuadExhaustSmoke(int x, int y, int z, short angle, int speed, int moving)
+	static void TriggerQuadExhaustSmoke(ItemInfo* quadBikeItem, int x, int y, int z, short angle, int speed, int moving)
 	{
 		auto* spark = GetFreeParticle();
 
+		auto quadBikeExhaustSmokeStartColor = PropertyHandler::Get(quadBikeItem, "VehicleSmokeStartColor", ScriptColor(0, 0, 0));
+		auto quadBikeExhaustSmokeEndColor = PropertyHandler::Get(quadBikeItem, "VehicleSmokeEndColor", ScriptColor(64, 64, 64));
+
 		spark->on = true;
-		spark->sR = 0;
-		spark->sG = 0;
-		spark->sB = 0;
+		spark->sR = quadBikeExhaustSmokeStartColor.GetR();
+		spark->sG = quadBikeExhaustSmokeStartColor.GetG();
+		spark->sB = quadBikeExhaustSmokeStartColor.GetB();
 
-		spark->dR = 96;
-		spark->dG = 96;
-		spark->dB = 128;
-
+		spark->dR = quadBikeExhaustSmokeEndColor.GetR();
+		spark->dG = quadBikeExhaustSmokeEndColor.GetG();
+		spark->dB = quadBikeExhaustSmokeEndColor.GetB();
 		if (moving)
 		{
 			spark->dR = (spark->dR * speed) / 32;
@@ -1218,7 +1221,7 @@ namespace TEN::Entities::Vehicles
 					if (quadBikeItem->Animation.Velocity.z < 64)
 					{
 						speed = 64 - quadBikeItem->Animation.Velocity.z;
-						TriggerQuadExhaustSmoke(pos.x, pos.y, pos.z, angle, speed, 1);
+						TriggerQuadExhaustSmoke(quadBikeItem, pos.x, pos.y, pos.z, angle, speed, 1);
 					}
 				}
 				else
@@ -1235,7 +1238,7 @@ namespace TEN::Entities::Vehicles
 					else
 						speed = 0;
 
-					TriggerQuadExhaustSmoke(pos.x, pos.y, pos.z, angle, speed, 0);
+					TriggerQuadExhaustSmoke(quadBikeItem, pos.x, pos.y, pos.z, angle, speed, 0);
 				}
 			}
 		}
