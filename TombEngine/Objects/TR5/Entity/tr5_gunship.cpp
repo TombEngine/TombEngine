075b0211f5bd0724733bf034c73793171722e15b
Nemoel-Tomo <tomo_669@hotmail.com>
2026-06-13 13:28:58 +0200

    update tr5_gunship.cpp

--- TombEngine/Objects/TR5/Entity/tr5_gunship.cpp
+++ TombEngine/Objects/TR5/Entity/tr5_gunship.cpp
@@ -3,187 +3,145 @@
 
 #include "Game/Animation/Animation.h"
 #include "Game/camera.h"
+#include "Game/collision/collide_item.h"
+#include "Game/collision/collide_room.h"
+#include "Game/control/box.h"
+#include "Game/control/control.h"
 #include "Game/control/los.h"
 #include "Game/effects/debris.h"
 #include "Game/effects/effects.h"
 #include "Game/items.h"
-#include "Game/Lara/lara.h"
+#include "Game/misc.h"
 #include "Game/Setup.h"
+#include "Game/Lara/lara.h"
+#include "Math/Geometry.h"
 #include "Objects/Generic/Object/objects.h"
 #include "Sound/sound.h"
 #include "Specific/level.h"
 
 using namespace TEN::Animation;
+using namespace TEN::Math;
 
 namespace TEN::Entities::Creatures::TR5
 {
+	constexpr int DEFAULT_TARGET_DISTANCE_SECTORS = 3;
+	constexpr int SHOT_COUNTER_MULTIPLIER_NO_LOS = 2;
+	constexpr int ROTOR_ACTIVE_THRESHOLD = 15;
+	constexpr int FIRE_FIRE_RATE = 4;
+
+	constexpr float SHOT_COUNTER_SPEED_DEFAULT = 1.0f;
+
 	int GunShipCounter = 0;
 
 	void ControlGunShip(short itemNumber)
 	{
 		auto* item = &g_Level.Items[itemNumber];
 
-		if (TriggerActive(item))
+		if (!TriggerActive(item))
+			return;
+
+		SoundEffect(SFX_TR4_HELICOPTER_LOOP, &item->Pose);
+
+		int targetDistanceSectors = item->TriggerFlags;
+		if (targetDistanceSectors <= 0)
+			targetDistanceSectors = DEFAULT_TARGET_DISTANCE_SECTORS;
+
+		const int targetDistance = targetDistanceSectors * 1024;
+
+		int dx = LaraItem->Pose.Position.x - item->Pose.Position.x;
+		int dy = LaraItem->Pose.Position.y - item->Pose.Position.y;
+		int dz = LaraItem->Pose.Position.z - item->Pose.Position.z;
+		float currentDistance = sqrtf(dx * dx + dy * dy + dz * dz);
+
+		Vector3 direction(
+			(float)dx,
+			(float)dy,
+			(float)dz
+		);
+
+		float directionLength = direction.Length();
+		if (directionLength > 0.0f)
+			direction /= directionLength;
+
+		GameVector origin = item->Pose.Position;
+		GameVector targetPos = LaraItem->Pose.Position;
+		bool los = LOS(&origin, &targetPos);
+
+		if (los && currentDistance > 2048.0f)
 		{
-			SoundEffect(SFX_TR4_HELICOPTER_LOOP, &item->Pose);
+			Vector3 vecOrigin = origin.ToVector3();
+			Vector3 vecTarget = targetPos.ToVector3();
 
-			auto pos = GameVector(
-				GetJointPosition(
-					LaraItem, 
-					LM_TORSO,
-					Vector3i(
-						(GetRandomControl() & 0x1FF) - 255,
-						(GetRandomControl() & 0x1FF) - 255,
-						(GetRandomControl() & 0x1FF) - 255)));
+			EulerAngles targetOrient = Geometry::GetOrientToPoint(vecOrigin, vecTarget);
+			targetOrient.z += ANGLE(180.0f);
 
-			auto target = pos;
+			const int MAX_PITCH = ANGLE(185.0);
+			if (targetOrient.x < -MAX_PITCH) targetOrient.x = -MAX_PITCH;
+			if (targetOrient.x > MAX_PITCH) targetOrient.x = MAX_PITCH;
 
-			if (!item->ItemFlags[0] && !item->ItemFlags[1] && !item->ItemFlags[2])
-			{
-				item->ItemFlags[0] = pos.x / 16;
-				item->ItemFlags[1] = pos.y / 16;
-				item->ItemFlags[2] = pos.z / 16;
-			}
+			const int TRACK_SPEED = 3;
+			float lerpAlpha = 1.0f / powf(2.0f, TRACK_SPEED);
 
-			pos.x = (pos.x + 80 * item->ItemFlags[0]) / 6;
-			pos.y = (pos.y + 80 * item->ItemFlags[1]) / 6;
-			pos.z = (pos.z + 80 * item->ItemFlags[2]) / 6;
+			if (item->Timer == 0)
+				lerpAlpha = 1.0f;
 
-			item->ItemFlags[0] = pos.x / 16;
-			item->ItemFlags[1] = pos.y / 16;
-			item->ItemFlags[2] = pos.z / 16;
-
-			if (item->TriggerFlags == 1)
-				item->Pose.Position.z += (pos.z - item->Pose.Position.z) / 32;
-			else
-				item->Pose.Position.x += (pos.x - item->Pose.Position.x) / 32;
-			item->Pose.Position.y += (pos.y - item->Pose.Position.y - 256) / 32;
-
-			GameVector origin;
-			origin.x = GetRandomControl() + item->Pose.Position.x - 128;
-			origin.y = GetRandomControl() + item->Pose.Position.y - 128;
-			origin.z = GetRandomControl() + item->Pose.Position.z - 128;
-			origin.RoomNumber = item->RoomNumber;
-			bool los = LOS(&origin, &target);
-
-			target.x = 3 * pos.x - 2 * origin.x;
-			target.y = 3 * pos.y - 2 * origin.y;
-			target.z = 3 * pos.z - 2 * origin.z;
-			bool los2 = LOS(&origin, &target);
-
-			if (los)
-				GunShipCounter = 1;
-			else
-				GunShipCounter++;
-
-			if (GunShipCounter <= 15)
-				item->MeshBits |= 0x100;
-			else
-				item->MeshBits &= 0xFEFF;
-
-			if (GunShipCounter < 15)
-				SoundEffect(SFX_TR4_HK_FIRE, &item->Pose, SoundEnvironment::Land, 0.8f);
-
-			if (!(GlobalCounter & 1))
-				return AnimateItem(item);
-
-			Vector3i hitPos;
-			StaticMesh* hitMesh = nullptr;
-			int objOnLos = ObjectOnLOS2(&origin, &target, &hitPos, &hitMesh, GAME_OBJECT_ID::ID_LARA);
-
-			if (objOnLos == NO_LOS_ITEM || objOnLos < 0)
-			{
-				if (GunShipCounter >= 15)
-					return AnimateItem(item);
-
-				SpawnDynamicLight(
-					origin.x, origin.y, origin.z, 16,
-					(GetRandomControl() & 0x3F) + 96,
-					(GetRandomControl() & 0x1F) + 64,
-					0);
-
-				if (!los2)
-				{
-					TriggerRicochetSpark(target, 2 * GetRandomControl());
-				}
-
-				if (objOnLos < 0 && GetRandomControl() & 1)
-				{
-					if (Statics[hitMesh->Slot].shatterType != ShatterType::None)
-					{
-						ShatterObject(0, hitMesh, 64, target.RoomNumber, 0);
-						TestTriggers(hitMesh->Pose.Position.x, hitMesh->Pose.Position.y, hitMesh->Pose.Position.z, target.RoomNumber, true);
-						SoundEffect(GetShatterSound(hitMesh->Slot), &hitMesh->Pose);
-					}
-
-					TriggerRicochetSpark(GameVector(hitPos), 2 * GetRandomControl());
-				}
-			}
-			else
-			{
-				auto* hitItem = &g_Level.Items[objOnLos];
-
-				if (hitItem->ObjectNumber != ID_LARA)
-				{
-					if (hitItem->ObjectNumber >= ID_SMASH_OBJECT1 &&
-						hitItem->ObjectNumber <= ID_SMASH_OBJECT16)
-					{
-						ExplodeItemNode(hitItem, 0, 0, 128);
-						SmashObject(objOnLos);
-						KillItem(objOnLos);
-					}
-				}
-				else
-				{
-					SpawnDynamicLight(
-						origin.x, origin.y, origin.z,
-						16,
-						(GetRandomControl() & 0x3F) + 96,
-						(GetRandomControl() & 0x1F) + 64,
-						0);
-
-					DoBloodSplat(
-						origin.x, origin.y, origin.z,
-						(GetRandomControl() & 1) + 2,
-						2 * GetRandomControl(),
-						LaraItem->RoomNumber);
-
-					DoDamage(LaraItem, 20);
-				}
-			}
-
-			if (GunShipCounter < 15)
-			{
-				auto* spark = GetFreeParticle();
-
-				spark->on = 1;
-				spark->sR = spark->dR = (GetRandomControl() & 0x7F) + -128;
-				spark->sG = (spark->dR / 2) + (GetRandomControl() & 0x7F);
-
-				if (spark->sG > spark->sR)
-					spark->sG = spark->sR;
-
-				spark->sB = 0;
-				spark->dB = 0;
-				spark->dR = 0;
-				spark->dG = 0;
-				spark->colFadeSpeed = 12;
-				spark->blendMode = BlendMode::Additive;
-				spark->fadeToBlack = 0;
-				spark->life = 12;
-				spark->sLife = 12;
-				spark->x = origin.x;
-				spark->y = origin.y;
-				spark->z = origin.z;
-				spark->xVel = 4 * (target.x - origin.x);
-				spark->yVel = 4 * (target.y - origin.y);
-				spark->zVel = 4 * (target.z - origin.z);
-				spark->friction = 0;
-				spark->maxYvel = 0;
-				spark->flags = SP_NONE;
-			}
-
-			AnimateItem(item);
+			item->Pose.Orientation.Lerp(targetOrient, lerpAlpha);
 		}
+
+		const int MOVE_SPEED = 84;
+
+		const int minimumDistance = BLOCK(item->TriggerFlags);
+		bool tooCloseToLara = currentDistance < minimumDistance;
+
+		CollisionInfo coll{};
+		CollideSolidStatics(item, &coll);
+		const bool blockedByWorld = coll.HitStatic;
+
+		if (!blockedByWorld)
+		{
+			float verticalFactor = 0.5f;
+			if (tooCloseToLara || currentDistance < targetDistance)
+			{
+				item->Pose.Position.x -= static_cast<int>(direction.x * MOVE_SPEED);
+				item->Pose.Position.y -= static_cast<int>(direction.y * MOVE_SPEED * verticalFactor);
+				item->Pose.Position.z -= static_cast<int>(direction.z * MOVE_SPEED);
+			}
+			else
+			{
+				item->Pose.Position.x += static_cast<int>(direction.x * MOVE_SPEED);
+				item->Pose.Position.y += static_cast<int>(direction.y * MOVE_SPEED * verticalFactor);
+				item->Pose.Position.z += static_cast<int>(direction.z * MOVE_SPEED);
+			}
+		}
+
+		if (los)
+		{
+			const float MAX_SHOT_RANGE = targetDistance + BLOCK(2);
+			
+			if (currentDistance <= MAX_SHOT_RANGE)
+			{
+				GunShipCounter = 1;
+
+				if (!(GlobalCounter & (FIRE_FIRE_RATE - 1)))
+				{
+					SoundEffect(SFX_TR4_HK_FIRE, &item->Pose, SoundEnvironment::Land, 0.8f);
+				}
+			}
+			else
+			{
+				GunShipCounter += (int)(SHOT_COUNTER_SPEED_DEFAULT * ROTOR_ACTIVE_THRESHOLD);
+			}
+		}
+		else
+		{
+			GunShipCounter += SHOT_COUNTER_MULTIPLIER_NO_LOS;
+		}
+
+		if (GunShipCounter <= ROTOR_ACTIVE_THRESHOLD)
+			item->MeshBits |= 0x100;
+		else
+			item->MeshBits &= 0xFEFF;
+
+		AnimateItem(item);
 	}
 }
