38174db1899e38b3d8f4af5f4e1347087e2606d9
Nemoel-Tomo <tomo_669@hotmail.com>
2026-06-20 20:38:12 +0200

    Update tr5_gunship.cpp

--- TombEngine/Objects/TR5/Entity/tr5_gunship.cpp
+++ TombEngine/Objects/TR5/Entity/tr5_gunship.cpp
@@ -41,6 +41,7 @@ namespace TEN::Entities::Creatures::TR5
 	constexpr int FIRE_RATE = 30;
 
 	constexpr float MOVEMENT_LERP_SPEED = 4.0f;
+	constexpr int INERTIA_FRAMES = 15;
 	constexpr float YAW_LERP_SPEED = 3.0f;
 	constexpr float PITCH_LERP_SPEED = 5.0f;
 	constexpr float BANK_LERP_SPEED = 3.0f;
@@ -93,10 +94,21 @@ namespace TEN::Entities::Creatures::TR5
 		const float hdz = LaraItem->Pose.Position.z - item->Pose.Position.z;
 		const float hLen = sqrtf(hdx * hdx + hdz * hdz);
 
-		// Zustand bestimmen
-		item->ItemFlags[6] = GunShipState::FOLLOW;
+		// Zustand bestimmen (ItemFlags[6] = Zustand, ItemFlags[4] = vorheriger Zustand, ItemFlags[5] = Inertia-Timer)
+		int currentState = GunShipState::FOLLOW;
 		if (hLen < minDistance)
-			item->ItemFlags[6] = GunShipState::EVADE_NEAR;
+			currentState = GunShipState::EVADE_NEAR;
+
+		// Trägheit: Wenn sich der Zustand geändert hat, Timer starten
+		int prevStates = item->ItemFlags[4];
+		int inertiaTimer = item->ItemFlags[5];
+		if (prevStates != currentState)
+		{
+			inertiaTimer = INERTIA_FRAMES;
+			item->ItemFlags[4] = currentState;
+		}
+
+		item->ItemFlags[6] = currentState;
 
 		// Beschleunigung/Verzögerung über ItemFlags speichern (ItemFlags[3] = currentSpeed * 1000)
 		float currentSpeed = fabsf((float)item->ItemFlags[3] / FLOATING_POINT_SCALE);
@@ -104,7 +116,7 @@ namespace TEN::Entities::Creatures::TR5
 
 		// Zielgeschwindigkeit berechnen — mit Distanzberücksichtigung
 		float targetSpeed = 0.0f;
-		switch (item->ItemFlags[6])
+		switch (currentState)
 		{
 		case GunShipState::FOLLOW:
 			if (hLen > maxShotsRange)
@@ -119,6 +131,14 @@ namespace TEN::Entities::Creatures::TR5
 			break;
 		}
 
+		// Trägheit: während der Inertia-Phase stark verzögern
+		if (inertiaTimer > 0)
+		{
+			targetSpeed *= 0.15f;
+			inertiaTimer--;
+			item->ItemFlags[5] = inertiaTimer;
+		}
+
 		// Geschwindigkeit sanft anpassen
 		const float speedAlpha = 1.0f / powf(2.0f, MOVEMENT_LERP_SPEED);
 		currentSpeed += (targetSpeed - currentSpeed) * speedAlpha;
@@ -128,111 +148,90 @@ namespace TEN::Entities::Creatures::TR5
 
 		const bool isMoving = currentSpeed > 1.0f;
 
-		// Zielposition berechnen
-		int targetX = item->Pose.Position.x;
 		int targetY = item->Pose.Position.y;
-		int targetZ = item->Pose.Position.z;
 		float pitchTarget = 0.0f;
 		float bankTarget = 0.0f;
 
+		// Berechne Ziel-Orientierung VOR der Bewegung für korrekte Vorwärtsrichtung
+		Vector3 vecOrigin = item->Pose.Position.ToVector3();
+		Vector3 vecTarget = LaraItem->Pose.Position.ToVector3();
+		EulerAngles targetOrient = Geometry::GetOrientToPoint(vecOrigin, vecTarget);
+
 		if (isMoving && hLen > 100.0f)
 		{
-			const float moveDist = currentSpeed / powf(2.0f, MOVEMENT_LERP_SPEED);
+			const float moveDist = currentSpeed;
 
-			switch (item->ItemFlags[6])
+			switch (currentState)
 			{
 			case GunShipState::FOLLOW:
-			{
-				// Bewegung entlang der Helisichtlinie via Geometry::TranslatePoint
-				auto targetPos = Geometry::TranslatePoint(item->Pose.Position.ToVector3(), item->Pose.Orientation, moveDist);
-				targetX = (int)targetPos.x;
-				targetZ = (int)targetPos.z;
+				{
+					// Ohne Offset: Nase zu Lara → TranslatePoint fliegt hin zu Lara
+					auto targetPos = Geometry::TranslatePoint(item->Pose.Position.ToVector3(), targetOrient, moveDist);
+					item->Pose.Position.x += (int)(targetPos.x - item->Pose.Position.x);
+					item->Pose.Position.z += (int)(targetPos.z - item->Pose.Position.z);
 
-				// Vorwrtpitch (nach unten = positiv)
-				pitchTarget = (float)DEG_TO_RAD(MAX_PITCH_DEG);
+					pitchTarget = (float)DEG_TO_RAD(MAX_PITCH_DEG);
+					targetY = (int)(LaraItem->Pose.Position.y - SECTOR_SIZE / 2);
 
-			// Seitliches Banken: Cross Product zwischen Helis Vorwärtsvektor und zum-Lara-Vektor
-			auto fwdVec = Vector3(
-				cosf(item->Pose.Orientation.x) * sinf(item->Pose.Orientation.y),
-				sinf(item->Pose.Orientation.x),
-				cosf(item->Pose.Orientation.x) * cosf(item->Pose.Orientation.y));
-			fwdVec.Normalize();
+					auto fwdVec = Vector3(
+						cosf(item->Pose.Orientation.x) * sinf(item->Pose.Orientation.y),
+						sinf(item->Pose.Orientation.x),
+						cosf(item->Pose.Orientation.x) * cosf(item->Pose.Orientation.y));
+					fwdVec.Normalize();
 
-			Vector3 toLara = LaraItem->Pose.Position.ToVector3() - item->Pose.Position.ToVector3();
-			toLara.y = 0.0f;
-			toLara.Normalize();
+					Vector3 toLaraNorm = LaraItem->Pose.Position.ToVector3() - item->Pose.Position.ToVector3();
+					toLaraNorm.y = 0.0f;
+					toLaraNorm.Normalize();
 
-			Vector3 crossProduct;
-			crossProduct.x = fwdVec.y * toLara.z - fwdVec.z * toLara.y;
-			crossProduct.y = fwdVec.z * toLara.x - fwdVec.x * toLara.z;
-			crossProduct.z = fwdVec.x * toLara.y - fwdVec.y * toLara.x;
-
-			bankTarget = DEG_TO_RAD(MAX_BANK_DEG) * crossProduct.y;
-
-			// Y-Hhe: in Lara-Nhe (negativ Y = oben)
-			targetY = (int)(LaraItem->Pose.Position.y - SECTOR_SIZE / 2);
+					float crossY = fwdVec.z * toLaraNorm.x - fwdVec.x * toLaraNorm.z;
+					bankTarget = DEG_TO_RAD(MAX_BANK_DEG) * crossY;
+				}
 				break;
-			}
 
 			case GunShipState::EVADE_NEAR:
-			{
-				// Zurckfliegen (entgegen der Helisichtlinie)
-				EulerAngles evadeOrient = item->Pose.Orientation;
-				evadeOrient.y += ANGLE(180.0f);
-				auto targetPos = Geometry::TranslatePoint(item->Pose.Position.ToVector3(), evadeOrient, moveDist);
-				targetX = (int)targetPos.x;
-				targetZ = (int)targetPos.z;
-
-				// Rckwertspitch (nach oben = negativ)
-				pitchTarget = -(float)DEG_TO_RAD(MAX_PITCH_DEG);
-
-				// Nach oben steigen (negativ Y = oben)
-				targetY -= (int)(FLY_UP_SPEED / powf(2.0f, MOVEMENT_LERP_SPEED));
-
-				// Seitliches Banken: Cross Product zwischen Helis Vorwärtsvektor und zum-Lara-Vektor
-				auto fwdVec = Vector3(
-					cosf(item->Pose.Orientation.x) * sinf(item->Pose.Orientation.y),
-					sinf(item->Pose.Orientation.x),
-					cosf(item->Pose.Orientation.x) * cosf(item->Pose.Orientation.y));
-				fwdVec.Normalize();
-
-				Vector3 toLara = LaraItem->Pose.Position.ToVector3() - item->Pose.Position.ToVector3();
-				toLara.y = 0.0f;
-				toLara.Normalize();
-
-				Vector3 crossProduct;
-				crossProduct.x = fwdVec.y * toLara.z - fwdVec.z * toLara.y;
-				crossProduct.y = fwdVec.z * toLara.x - fwdVec.x * toLara.z;
-				crossProduct.z = fwdVec.x * toLara.y - fwdVec.y * toLara.x;
-
-				bankTarget = DEG_TO_RAD(MAX_BANK_DEG) * crossProduct.y;
-
-				// Mindesthe ber Boden
-				FloorInfo* floorCheck = GetFloor(targetX, targetY, targetZ, &item->RoomNumber);
-				if (floorCheck != nullptr)
 				{
-					const int floorH = GetFloorHeight(floorCheck, targetX, targetY, targetZ);
-					if (floorH != NO_VALUE)
+					EulerAngles evadeOrient = targetOrient;
+					evadeOrient.y += ANGLE(180.0f);
+					auto targetPos = Geometry::TranslatePoint(item->Pose.Position.ToVector3(), evadeOrient, moveDist);
+					Vector3 delta = targetPos - item->Pose.Position.ToVector3();
+					item->Pose.Position.x += (int)delta.x;
+					item->Pose.Position.z += (int)delta.z;
+
+					pitchTarget = -(float)DEG_TO_RAD(MAX_PITCH_DEG);
+					targetY -= (int)(FLY_UP_SPEED * 0.5f);
+
+					auto fwdVec = Vector3(
+						cosf(item->Pose.Orientation.x) * sinf(item->Pose.Orientation.y),
+						sinf(item->Pose.Orientation.x),
+						cosf(item->Pose.Orientation.x) * cosf(item->Pose.Orientation.y));
+					fwdVec.Normalize();
+
+					Vector3 toLara = LaraItem->Pose.Position.ToVector3() - item->Pose.Position.ToVector3();
+					toLara.y = 0.0f;
+					toLara.Normalize();
+
+					float bankCrossY = fwdVec.z * toLara.x - fwdVec.x * toLara.z;
+					bankTarget = DEG_TO_RAD(MAX_BANK_DEG) * bankCrossY;
+
+					FloorInfo* floorCheck = GetFloor(item->Pose.Position.x, item->Pose.Position.y, item->Pose.Position.z, &item->RoomNumber);
+					if (floorCheck != nullptr)
 					{
-						auto& heliFrame = GetFrame(*item);
-						const float minH = (int)(floorH - SECTOR_SIZE / 2);
-						if (targetY < minH)
-							targetY = minH;
+						const int floorH = GetFloorHeight(floorCheck, item->Pose.Position.x, item->Pose.Position.y, item->Pose.Position.z);
+						if (floorH != NO_VALUE)
+						{
+							auto& heliFrame = GetFrame(*item);
+							const float minH = (int)(floorH - SECTOR_SIZE / 2);
+							if (targetY < minH)
+								targetY = minH;
+						}
 					}
 				}
 				break;
-			}
 
 			default:
 				break;
 			}
 		}
-
-		// Position lerp-en
-		const float posLerpAlpha = 1.0f / powf(2.0f, MOVEMENT_LERP_SPEED);
-		item->Pose.Position.x += (int)((targetX - item->Pose.Position.x) * posLerpAlpha);
-		item->Pose.Position.y += (int)((targetY - item->Pose.Position.y) * posLerpAlpha);
-		item->Pose.Position.z += (int)((targetZ - item->Pose.Position.z) * posLerpAlpha);
 
 		// Post-position collision: Wall sliding
 		CollisionInfo coll{};
@@ -257,7 +256,6 @@ namespace TEN::Entities::Creatures::TR5
 		item->ItemFlags[1] = (int)(currentPitch * FLOATING_POINT_SCALE);
 		item->ItemFlags[2] = (int)(currentBankAngle * FLOATING_POINT_SCALE);
 
-		// Wenn nicht bewegt, Neigung abbauen
 		if (!isMoving)
 		{
 			currentPitch *= 0.95f;
@@ -266,27 +264,21 @@ namespace TEN::Entities::Creatures::TR5
 			item->ItemFlags[2] = (int)(currentBankAngle * FLOATING_POINT_SCALE);
 		}
 
-		// Orientation — yaw from GetOrientToPoint, controlled pitch/roll
-		Vector3 vecOrigin = item->Pose.Position.ToVector3();
-		Vector3 vecTarget = LaraItem->Pose.Position.ToVector3();
-
-		EulerAngles targetOrient = Geometry::GetOrientToPoint(vecOrigin, vecTarget);
-		targetOrient.y += ANGLE(180.0f);
-
 		constexpr int TRACK_SPEED = 3;
 		float lerpAlpha = 1.0f / powf(2.0f, TRACK_SPEED);
 		if (item->ItemFlags[0] == 1)
 			lerpAlpha = 1.0f;
 
-		EulerAngles lerpResult = EulerAngles::Lerp(item->Pose.Orientation, targetOrient, lerpAlpha);
+		EulerAngles lerpResult = targetOrient;
+		lerpResult.y += ANGLE(180.0f);
 
 		constexpr float RAD_TO_SHORTS = (float)(65536.0 / (2.0 * PI));
 		lerpResult.x = (short)(currentPitch * RAD_TO_SHORTS);
 		lerpResult.z = (short)(currentBankAngle * RAD_TO_SHORTS);
 
-		item->Pose.Orientation = lerpResult;
+		EulerAngles lerpResult2 = EulerAngles::Lerp(item->Pose.Orientation, lerpResult, lerpAlpha);
+		item->Pose.Orientation = lerpResult2;
 
-		// Ceiling correction
 		FloorInfo* floorInfo = GetFloor(item->Pose.Position.x, item->Pose.Position.y, item->Pose.Position.z, &item->RoomNumber);
 		int ceilingHeight = NO_VALUE;
 		if (floorInfo != nullptr)
@@ -311,7 +303,6 @@ namespace TEN::Entities::Creatures::TR5
 			}
 		}
 
-		// Mesh-sicherheitsabstand zum Boden
 		FloorInfo* floorCheck = GetFloor(item->Pose.Position.x, item->Pose.Position.y, item->Pose.Position.z, &item->RoomNumber);
 		if (floorCheck != nullptr)
 		{
@@ -327,7 +318,6 @@ namespace TEN::Entities::Creatures::TR5
 			}
 		}
 
-		// --- Shot counter & mesh control ---
 		item->ItemFlags[0]++;
 
 		const bool isInRange = hLen <= maxShotsRange;
