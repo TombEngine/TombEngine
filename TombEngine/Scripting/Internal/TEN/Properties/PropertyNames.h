01ab13d5638cee4c6aaf52c6ecc547a223013bb2
Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
2026-07-17 05:42:07 +0100

    Vehicle Properties (#2057)
    * Add toggle options for speedboat rendering effects
    
    * Revert "Add toggle options for speedboat rendering effects"
    
    This reverts commit 975a40c0c5b2f5cde1ae5d6e79b29fb8f117b76c.
    
    * Redo implentation: Add toggle options for speedboat rendering effects
    
    * Add speed modifier / standardise vehicle properties
    
    * Update speedboat.cpp
    
    * Add land vehicle wake effect toggle
    
    * Remove speed modifier
    
    I cannot work out how to apply it properly to snowmobile so dropping it entirely for now.
    
    * Add mist effect property for rubberboat
    
    * Add UPV mist property
    
    * Add light color and intensity for UPV
    
    * Add Light radius / falloff / distance  / shadow property
    
    * Naming clear-up
    
    Better to keep the declarations named specifically for the vehicle, and use the general name in the property
    
    * Add quadbike smoke start/end color properties
    
    * Change quadbike exhaust to screen blending and add haze
    
    * Add motorbike light parameters / renames
    
    * Add Jeep headlight and properties
    
    * Add optional jeep headlight mesh
    
    * Correct property name
    
    * Fix speedboat Faster not using fast velocity cap
    
    * Fix rubber boat snow mist color overflow
    
    * Fix jeep light intensity using motorbike property name
    
    * Enable jeep light by default to match headlight mesh
    
    * Use UCHAR_MAX in UPV light color
    
    * Unify motorbike light properties under Motorbike prefix
    
    * Use GetHash for vehicle property getters
    
    * Use OCB-priority default for speedboat mist and foam
    
    * Pre-cache vehicle property hashes as named constants
    
    * Reorganise vehicle properties
    
    * Update PropertyNames.h
    
    ---------
    
    Co-authored-by: MontyTRC89 <montyhammet@hotmail.it>

--- TombEngine/Scripting/Internal/TEN/Properties/PropertyNames.h
+++ TombEngine/Scripting/Internal/TEN/Properties/PropertyNames.h
@@ -19,3 +19,33 @@ static const auto PropName_Delay				= GetHash("Delay");
 static const auto PropName_AttackType	= GetHash("AttackType");
 static const auto PropName_MeshID		= GetHash("MeshID");
 static const auto PropName_PrimaryColor = GetHash("PrimaryColor");
+
+// Effect property names.
+
+static const auto PropName_PointLightEnabled	= GetHash("PointLightEnabled");
+static const auto PropName_PointLightCastShadow = GetHash("PointLightCastShadow");
+static const auto PropName_PointLightColor		= GetHash("PointLightColor");
+static const auto PropName_PointLightFalloff	= GetHash("PointLightFalloff");
+
+static const auto PropName_SpotLightEnabled		= GetHash("SpotLightEnabled");
+static const auto PropName_SpotLightCastShadow	= GetHash("SpotLightCastShadow");
+static const auto PropName_SpotLightColor		= GetHash("SpotLightColor");
+static const auto PropName_SpotLightDistance	= GetHash("SpotLightDistance");
+static const auto PropName_SpotLightFalloff		= GetHash("SpotLightFalloff");
+static const auto PropName_SpotLightIntensity	= GetHash("SpotLightIntensity");
+static const auto PropName_SpotLightRadius		= GetHash("SpotLightRadius");
+
+// Vehicle property names.
+
+static const auto PropName_VehicleFoam				= GetHash("VehicleFoamEnable");
+
+static const auto PropName_VehicleMist				= GetHash("VehicleMistEnable");
+static const auto PropName_VehicleMistStartColor	= GetHash("VehicleMistStartColor");
+static const auto PropName_VehicleMistEndColor		= GetHash("VehicleMistEndColor");
+
+static const auto PropName_VehicleSmokeStartColor	= GetHash("VehicleSmokeStartColor");
+static const auto PropName_VehicleSmokeEndColor		= GetHash("VehicleSmokeEndColor");
+
+static const auto PropName_VehicleWake				= GetHash("VehicleWakeEnable");
+static const auto PropName_VehicleWakeStartColor	= GetHash("VehicleWakeStartColor");
+static const auto PropName_VehicleWakeEndColor		= GetHash("VehicleWakeEndColor");
